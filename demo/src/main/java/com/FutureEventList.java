0e8e779b23621fd09bd018dae7b87962da37d563
Fran <franciscoquiroga364@gmail.com>
2026-03-30 12:26:47 -0300

    SUBO CLASE FEL

---
+++ demo/src/main/java/com/FutureEventList.java
@@ -0,0 +1,34 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FutureEventList {
+    private List<Event> fel;
+    private int comparator; 
+    public FutureEventList(){
+        this.fel = new ArrayList<>();
+        this.comparator = new Comparator<Event>();
+    }
+
+    @Override
+    public int compare (Event e1, Event e2){
+        if (e1.clock() < e2.clock()){
+            return -1; //e1 < e2       
+        }else if (e1.clock() > e2.clock()){
+            return 1; //e1 > e2
+        }else{
+            if (e1.order() < e2.order() ) {
+                return -1; //si e1 < e2
+            }else if(e1.order() > e2.order()){
+                return 1; //si e1 > e2
+            }else{
+                return 0; //SI SON IGUALES TOMA PRIORIDAD LA SALIDA
+            }
+
+        }
+        
+        
+    }
+       
+}
