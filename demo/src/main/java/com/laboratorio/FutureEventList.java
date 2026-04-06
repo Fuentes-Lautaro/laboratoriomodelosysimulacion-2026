da90db121d775f0eff85982e6b09e7eafe689bc5
Fran <franciscoquiroga364@gmail.com>
2026-04-06 11:04:20 -0300

    FEL ARREGLADA

--- demo/src/main/java/com/laboratorio/FutureEventList.java
+++ demo/src/main/java/com/laboratorio/FutureEventList.java
@@ -2,7 +2,7 @@ package com.laboratorio;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Comparator;
 
 
 public class FutureEventList {
@@ -11,25 +11,27 @@ public class FutureEventList {
 
     public FutureEventList(){
         this.fel = new ArrayList<>();
-        this.comparator = new Comparator<Event>();
-    }
-
-    @Override
-    public int compare (Event e1, Event e2){
-        if (e1.clock() < e2.clock()){
-            return -1; //e1 < e2       
-        }else if (e1.clock() > e2.clock()){
-            return 1; //e1 > e2
-        }else{
-            if (e1.order() < e2.order() ) {
-                return -1; //si e1 < e2
-            }else if(e1.order() > e2.order()){
-                return 1; //si e1 > e2
-            }else{
-                return 0; //SI SON IGUALES TOMA PRIORIDAD LA SALIDA
+        this.comparator = new Comparator<Event>(){
+            @Override
+            public int compare (Event e1, Event e2){
+                 if (e1.clock() < e2.clock()){
+                    return -1; //e1 < e2       
+                }else if (e1.clock() > e2.clock()){
+                    return 1; //e1 > e2
+                }else{
+                    if (e1.order() < e2.order() ) {
+                    return -1; //si e1 < e2
+                }else if(e1.order() > e2.order()){
+                    return 1; //si e1 > e2
+                }else{
+                    return 0; //SI SON IGUALES TOMA PRIORIDAD LA SALIDA
             }
         }
     }
+        };
+    }
+
+    
     
     public void insert(Event e){
         this.fel.add(e);
@@ -44,7 +46,7 @@ public class FutureEventList {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (Event e : this.fel) {
-            sb.append(String.format(("%s: %f\n", e.getClass().getSimpleName(), e.clock()));)
+            sb.append(String.format("%s: %f\n", e.getClass().getSimpleName(), e.clock()));
         }
         return sb.toString();
     }
