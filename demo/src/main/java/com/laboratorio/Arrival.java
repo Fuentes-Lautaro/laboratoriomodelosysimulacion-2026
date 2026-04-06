9a8cd387688974de5b9356cfd585019e596984c4
Lauty <fuenteslautaro2808@gmail.com>
2026-04-06 10:47:50 -0300

    Arrego arrival

--- demo/src/main/java/com/laboratorio/Arrival.java
+++ demo/src/main/java/com/laboratorio/Arrival.java
@@ -1,18 +1,20 @@
 package com.laboratorio;
 
 public class Arrival implements Event {
-    private double clock;
-    private int order;
+    private final double clock;
+    private final int order;
 
     public Arrival(Double clock) {
         this.clock = clock;
         this.order = 10;
     }
 
+    @Override
     public double getClock(){
         return clock;
     }
 
+    @Override
     public int getOrder() {
         return order;
     }
@@ -21,12 +23,12 @@ public class Arrival implements Event {
     public void planificate(FutureEventList fel, Server server){
         if (server == null){ //CONSULTO SI SERVER ESTA OCUPADO
             // server = 
-            int tS = tiempoSalida(); //GENERO UN TIEMPO DE SERVICIO
+            int tS = DuracionInspeccion.tiempoSalida(); //GENERO UN TIEMPO DE SERVICIO
             fel.insert(new EndOfService(this.clock + tS)); //INSERTO EL EVENTO SALIDA DEL ELEMENTO ACTUAL
         }else{
             //agrego el evento a la cola
         }
-        int tea = tiempoEntreArribos(); //GENERO UN NUEVO TIEMPO ENTRE ARRIBOS
+        int tea = TiempoEntreArribos.tiempoEntreArribos(); //GENERO UN NUEVO TIEMPO ENTRE ARRIBOS
         fel.insert(new Arrival(this.clock + tea)); //INSERTO EL NUEVO EVENTO DE ARRIBO
 
         //COLECCIONO ESTADISTICAS
