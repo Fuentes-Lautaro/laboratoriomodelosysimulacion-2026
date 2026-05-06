caedd8fb80a2fce73cb2331102f06467dbe891ed
Guada <guadalupeabigailsuarez@gmail.com>
2026-05-06 16:50:57 -0300

    Agregue exponencial, uniforme y normal

--- demo/src/main/java/com/laboratorio/dominio/Randomizer.java
+++ demo/src/main/java/com/laboratorio/dominio/Randomizer.java
@@ -1,8 +1,22 @@
 package com.laboratorio.dominio;
+import java.util.Random;
 
 public class Randomizer {
+    private static Random random= new Random();
     
     public static double nroRandom() {
-        return Math.random(); 
+        return random.nextDouble();
+    }
+
+    public static double exponencial(double mu) {
+    return -mu * Math.log(1.0 - nroRandom());
+    }
+
+    public static double uniforme(double a, double b) {
+    return a + (b - a) * nroRandom();
+    }
+
+    public static double normal(double mu, double sigma){
+    return mu + (sigma * random.nextGaussian());
     }
 }
