fe9e33567b37667920be8a3cdebba79a456a1fb6
Guada <guadalupeabigailsuarez@gmail.com>
2026-05-06 19:01:37 -0300

    Agrego empirica

--- demo/src/main/java/com/laboratorio/dominio/Randomizer.java
+++ demo/src/main/java/com/laboratorio/dominio/Randomizer.java
@@ -19,4 +19,16 @@ public class Randomizer {
     public static double normal(double mu, double sigma){
     return mu + (sigma * random.nextGaussian());
     }
-}
+
+    public static int empiric(double[] probabilities) {
+        double r = nroRandom();
+        double cumulative = 0.0;
+        for (int i = 0; i < probabilities.length; i++) {
+            cumulative += probabilities[i];
+            if (r < cumulative) {
+                return i;
+            }
+        }
+        return probabilities.length - 1; 
+    }
+} 
